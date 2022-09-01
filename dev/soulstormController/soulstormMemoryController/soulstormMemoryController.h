6a0932c14da9a5d10c87b7f4724ad1ca50116807
Evgeny <dubinaxdd@gmail.com>
2022-09-01 12:20:17 +0500

    Чуток переделана сборка, добавлен забытый хэдер

---
+++ dev/soulstormController/soulstormMemoryController/soulstormMemoryController.h
@@ -0,0 +1,35 @@
+#ifndef SOULSTORMMEMORYCONTROLLER_H
+#define SOULSTORMMEMORYCONTROLLER_H
+
+#include <QObject>
+#include <QDebug>
+#include <QTextCodec>
+
+#include <Windows.h>
+#include <logger.h>
+#include <settingsController.h>
+
+class SoulstormMemoryController : public QObject
+{
+    Q_OBJECT
+public:
+    explicit SoulstormMemoryController(SettingsController* settingsController, QObject *parent = nullptr);
+
+    void setSoulstormHwnd(HWND newSoulstormHwnd);
+
+public slots:
+    void onNoFogStateChanged(bool state);
+    void onSsLaunchStateChanged(bool state);
+    void onSettingsLoaded();
+signals:
+
+private:
+    bool m_ssLaunchState = false;
+    HWND m_soulstormHwnd = NULL;
+    bool targetNoFog = false;
+    bool currentNoFog = false;
+    bool force = false; // применить изменения принудительно
+    SettingsController* m_settingsController;
+};
+
+#endif // SOULSTORMMEMORYCONTROLLER_H
