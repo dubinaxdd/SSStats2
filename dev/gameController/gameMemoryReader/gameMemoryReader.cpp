adc64feff092d8064540e56f213f825f6e223a95
Evgeny <dubinaxdd@gmail.com>
2025-10-31 11:08:40 +0500

    Фиксы для многопоточности при поиске Ид сессии и пропущенных игроков

--- dev/gameController/gameMemoryReader/gameMemoryReader.cpp
+++ dev/gameController/gameMemoryReader/gameMemoryReader.cpp
@@ -1,395 +1,20 @@
 #include <gameMemoryReader.h>
 #include <QVariantList>
 #include <QDebug>
-//#include <iostream>
-//#include <fstream>
+
 #include <QTextCodec>
 #include <logger.h>
 #include <ppl.h>
 
-//#define SCAN_STEAM_PLAYERS_INTERVAL 3000
-
-#define SCAN_STEAM_PLAYERS_INTERVAL 1000
 
 using namespace std;
 
 GameMemoryReader::GameMemoryReader(QObject *parent)
     : QObject(parent)
 {
-    m_scanTimer = new QTimer(this);
-    m_scanTimer->setInterval(SCAN_STEAM_PLAYERS_INTERVAL);
-
-    m_scanTimer->setSingleShot(true);
-
-    QObject::connect(m_scanTimer, &QTimer::timeout, this, &GameMemoryReader::refreshSteamPlayersInfo, Qt::QueuedConnection );
-    // m_scanTimer->start();
-}
-
-void GameMemoryReader::refreshSteamPlayersInfo()
-{
-
-    QTextCodec *codec = QTextCodec::codecForName("UTF-8");
-    QString ss = codec->toUnicode("Dawn of War: Soulstorm");
-    LPCWSTR lps = (LPCWSTR)ss.utf16();
-
-    m_gameHwnd = FindWindowW(NULL, lps);
-
-    if(!m_gameHwnd)
-        return;
-
-    DWORD PID;
-    GetWindowThreadProcessId(m_gameHwnd, &PID);
-    //qDebug() << "PID = " << PID;
-
-    // Получение дескриптора процесса
-    HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, PID);
-    if(hProcess==nullptr)
-        return;
-
-    //qInfo(logInfo()) << "Scan started";
-
-
-    QMap<QString, PlayerInfoFromDowServer> allPlayersInfo;
-
-    int playersCount = 0;
-
-    QByteArray buffer(/*30400*/100000, 0);
-
-
-   //unsigned long ptr1Count = 160116800;
-    unsigned long ptr1Count = /*100000000*/0x00000000;
-    while (ptr1Count < /*200000000*/0x7FFE0000)
-    {
-
-        SIZE_T bytesRead = 0;
-
-        // Если функция вернула не ноль, то продолжим цикл
-        if(!ReadProcessMemory(hProcess, (LPCVOID)ptr1Count, buffer.data(), /*30400*/100000 , &bytesRead))
-        {
-            if(GetLastError()!=299)
-            {
-                qDebug() << "Could not read process memory" << ptr1Count << GetLastError();
-                continue;
-            }
-        }
-
-        int playerPosition = 0;
-
-        //Ищем игроков в патиблоке
-        for (int i = 100; i < static_cast<int>(bytesRead) - 300; i++)
-        {
-            bool match = false;
-
-            for (int j = 0; j < static_cast<int>(sizeof(steamHeader)); j++)
-            {
-                if (buffer.at(i + j) != steamHeader[j])
-                {
-                    match = false;
-                    break;
-                }
-                else
-                    match = true;
-            }
-
-            if (!match)
-                continue;
-
-
-            bool match2 = false;
-
-            //Ищем постфикс байта с количеством игроков по первой маске
-            if (allPlayersInfo.count() == 0)
-            {
-
-                for (int k = i - 100; k < i; k++)
-                {
-
-                    for (int t = 0; t < static_cast<int>(sizeof(playresCountPostfix)); t++)
-                    {
-                        if (buffer.at(k + t) != playresCountPostfix[t])
-                        {
-                            match2= false;
-                            break;
-                        }
-                        else
-                            match2 = true;
-
-                    }
-
-                    if (match2)
-                    {
-                        //Дергаем байт с количеством игроков
-                        playersCount = QString::fromUtf8((char*)buffer.mid(k - 1, 1).data(), 1).toLocal8Bit().toHex().toInt();
-                        //qDebug() << Qt::hex << ptr1Count + k - 1;
-                        //qDebug() << playersCount;
-                        break;
-                    }
-                    else
-                        playersCount = 0;
-                }
-            }
-            //Ищем постфикс байта с количеством игроков по второй маске
-           if(!match2)
-            {
-                if (allPlayersInfo.count() == 0)
-                {
-
-                    for (int k = i - 100; k < i; k++)
-                    {
-
-                        for (int t = 0; t < static_cast<int>(sizeof(playresCountPostfix2)); t++)
-                        {
-                            if (buffer.at(k + t) != playresCountPostfix2[t])
-                            {
-                                match2= false;
-                                break;
-                            }
-                            else
-                                match2 = true;
-                        }
-
-                        if (match2)
-                        {
-                            if(buffer.at(k + sizeof(playresCountPostfix2)) == 0)
-                            {
-                                match2 = false;
-                                playersCount = 0;
-
-                            }
-                            else
-                            {
-                                //Дергаем байт с количеством игроков
-                                playersCount = QString::fromUtf8((char*)buffer.mid(k - 1, 1).data(), 1).toLocal8Bit().toHex().toInt();
-                                //qDebug() << Qt::hex << ptr1Count + k - 1;
-                                //qDebug() << playersCount;
-                                break;
-                            }
-                        }
-                        else
-                            playersCount = 0;
-                    }
-                }
-            }
-
-            if(playersCount == 0 || playersCount > 8)
-                continue;
-
-            int nickPos = i + 56;
-
-            if (buffer.at(nickPos) < 50 &&
-                    buffer.at(nickPos) > 0 &&
-                    buffer.at(nickPos + 1) == 0 &&
-                    buffer.at(nickPos + 2) == 0 &&
-                    buffer.at(nickPos + 3) == 0 &&
-                    buffer.at(nickPos - 1) == 0 &&
-                    buffer.at(nickPos - 2) == 0 &&
-                    buffer.at(nickPos - 3) == 0 &&
-                    buffer.at(nickPos - 4) == 0 &&
-                    buffer.at(nickPos+4+buffer.at(nickPos)*2)   == 0 &&
-                    buffer.at(nickPos+4+buffer.at(nickPos)*2+1) == 0 &&
-                    buffer.at(nickPos+4+buffer.at(nickPos)*2+2) == 0 &&
-                    buffer.at(nickPos+4+buffer.at(nickPos)*2+3) == 0)
-            {
-
-                QString steamIdStr = QString::fromUtf16((ushort*)buffer.mid(i + 18, 34).data()).left(17);
-
-                if(!steamIdStr.contains(QRegExp("^[0-9]{17}$")))
-                    continue;
-
-                QString nick = QString::fromUtf16((ushort*)buffer.mid(nickPos + 4, buffer.at(nickPos) * 2).data()).left(buffer.at(nickPos));
-
-                //qDebug() << "Player found:" << nick << QString("http://steamcommunity.com/profiles/"+steamIdStr) << "at address" << ptr1Count;
-
-                //QString fullString = QString::fromUtf8((char*)buffer.mid(nickPos + 4 + (nick.length()*2), 100).data(), 100);
-                //qDebug() << Qt::hex << fullString.toLocal8Bit().toHex();
-
-                QString closeConnectionFlag = QString::fromUtf8((char*)buffer.mid(nickPos + 4 + (nick.length()*2) + 29, 15).data(), 15);
-                //QString postfixFlag = QString::fromStdString(closeConnectionFlag.toLocal8Bit().toHex().toStdString());
-                //qDebug() << closeConnectionFlag;
-
-                //TODO: это тоже можно использовать как фильтр
-                //4E 6F 20 6C 6F 6E 67 65 72 20 63 6F 6E 6E 65 63 74 65 64 20 74 6F 20 68 6F 73 74 00
-                //No longer connection to host
-
-                if(!allPlayersInfo.contains(steamIdStr)){
-
-                    PlayerInfoFromDowServer newPlayerInPaty;
-
-                    newPlayerInPaty.steamId = steamIdStr;
-                    newPlayerInPaty.name = nick;
-                    newPlayerInPaty.position = playerPosition;
-                    playerPosition++;
-
-                    if(closeConnectionFlag == "CloseNetSession" || closeConnectionFlag == "No longer conne")
-                        //newPlayerInPaty.closeConnection = true;
-                        continue;
-
-                    allPlayersInfo.insert(steamIdStr, newPlayerInPaty);
-                }
-                else
-                {
-                    if(allPlayersInfo.value(steamIdStr).name != nick)
-                        allPlayersInfo[steamIdStr].name = nick;
-
-                    if(closeConnectionFlag == "CloseNetSession" || closeConnectionFlag == "No longer conne")
-                        //allPlayersInfo[steamIdStr].closeConnection = true;
-                        continue;
-                }
-            }
-        }
-
-        if (allPlayersInfo.count() > 0 && playersCount > 1 && playersCount <= 8)
-            break;
-
-
-        playersCount = 0;
-        allPlayersInfo.clear();
-        ptr1Count += /*30400*/100000;
-    }
-
-
-    QList<PlayerInfoFromDowServer> playersList;
-
-    for(int i = 0; i < allPlayersInfo.values().size(); i++)
-    {
-        if (!allPlayersInfo.values().at(i).closeConnection)
-            playersList.append(allPlayersInfo.values().at(i));
-    }
-
-    //qDebug() << "==============================================================";
-    //for(int i = 0; i < allPlayersInfo.values().size(); i++)
-        //qDebug() << playersList.name;
-
-
-    emit sendSteamPlayersInfoMap(playersList, playersCount);
-
-    // qInfo(logInfo()) << "Scan finished";
-}
-
-
-QTimer *GameMemoryReader::scanTimer() const
-{
-    return m_scanTimer;
-}
-
-void GameMemoryReader::setSoulstormHwnd(HWND newSoulstormHwnd)
-{
-    m_gameHwnd = newSoulstormHwnd;
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-void GameMemoryReader::findPlayerBySsId(int ssId, int playerPosititon)
-{
-    QTextCodec *codec = QTextCodec::codecForName("UTF-8");
-    QString ss = codec->toUnicode("Dawn of War: Soulstorm");
-    LPCWSTR lps = (LPCWSTR)ss.utf16();
-
-    m_gameHwnd = FindWindowW(NULL, lps);
-
-    if(!m_gameHwnd)
-        return;
-
-    DWORD PID;
-    GetWindowThreadProcessId(m_gameHwnd, &PID);
-
-    // Получение дескриптора процесса
-    HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, PID);
-    if(hProcess==nullptr)
-        return;
-
-    PlayerInfoFromDowServer playerInfo;
-
-    int playersCount = 0;
-
-    QByteArray buffer(/*30400*/100000, 0);
-
-
-   //unsigned long ptr1Count = 160116800;
-    unsigned long ptr1Count = /*100000000*/0x00000000;
-    while (ptr1Count < /*200000000*/0x7FFE0000)
-    {
-
-        SIZE_T bytesRead = 0;
-
-        // Если функция вернула не ноль, то продолжим цикл
-        if(!ReadProcessMemory(hProcess, (LPCVOID)ptr1Count, buffer.data(), /*30400*/100000 , &bytesRead))
-        {
-            if(GetLastError()!=299)
-            {
-                qDebug() << "Could not read process memory" << ptr1Count << GetLastError();
-                continue;
-            }
-        }
-
-        int playerPosition = 0;
-
-        //Ищем игроков в патиблоке
-        for (int i = 100; i < static_cast<int>(bytesRead) - 300; i++)
-        {
-            bool match = false;
-
-            for (int j = 0; j < static_cast<int>(sizeof(steamHeader)); j++)
-            {
-                if (buffer.at(i + j) != steamHeader[j])
-                {
-                    match = false;
-                    break;
-                }
-                else
-                    match = true;
-            }
-
-            if (!match)
-                continue;
-
-            int nickPos = i + 56;
-
-            if (buffer.at(nickPos) < 50 &&
-                    buffer.at(nickPos) > 0 &&
-                    buffer.at(nickPos + 1) == 0 &&
-                    buffer.at(nickPos + 2) == 0 &&
-                    buffer.at(nickPos + 3) == 0 &&
-                    buffer.at(nickPos - 1) == 0 &&
-                    buffer.at(nickPos - 2) == 0 &&
-                    buffer.at(nickPos - 3) == 0 &&
-                    buffer.at(nickPos - 4) == 0 &&
-                    buffer.at(nickPos+4+buffer.at(nickPos)*2)   == 0 &&
-                    buffer.at(nickPos+4+buffer.at(nickPos)*2+1) == 0 &&
-                    buffer.at(nickPos+4+buffer.at(nickPos)*2+2) == 0 &&
-                    buffer.at(nickPos+4+buffer.at(nickPos)*2+3) == 0)
-            {
-
-                QByteArray readedSsIdByteArray = buffer.mid(i - 8, 4).data();
-
-                int readedSsId = *reinterpret_cast<int*>(readedSsIdByteArray.data());
-
-                if (readedSsId != ssId)
-                    continue;
-
-                QString steamIdStr = QString::fromUtf16((ushort*)buffer.mid(i + 18, 34).data()).left(17);
-
-                if(!steamIdStr.contains(QRegExp("^[0-9]{17}$")))
-                    continue;
-
-                QString nick = QString::fromUtf16((ushort*)buffer.mid(nickPos + 4, buffer.at(nickPos) * 2).data()).left(buffer.at(nickPos));
-
-                playerInfo.steamId = steamIdStr;
-                playerInfo.name = nick;
-                playerInfo.position = playerPosititon;
-
-                emit sendSteamPlayerInfoForHostedGame(playerInfo);
-
-                return;
-
-            }
-        }
-
-        ptr1Count += /*30400*/100000;
-    }
+    m_ignoredPlayersIdFinded = false;
+    m_dataFinded = false;
+    m_abort = false;
 }
 
 void GameMemoryReader::findSessionId()
@@ -417,6 +42,9 @@ void GameMemoryReader::findSessionId()
 
         concurrency::parallel_for_each(numbers.begin(), numbers.end(), [&](DWORD64 n) {
             sessionId = findDefinitiveEditionSessionId(n, n+step, hProcess);
+
+            if(m_abort == true)
+                return;
 
             if (!m_dataFinded && !sessionId.isEmpty())
             {
@@ -451,9 +79,7 @@ void GameMemoryReader::findSessionId()
 
 void GameMemoryReader::abort()
 {
-    m_playersSteamScannerMutex.lock();
     m_abort = true;
-    m_playersSteamScannerMutex.unlock();
 }
 
 void GameMemoryReader::findIgnoredPlayersId(QStringList playersIdList)
@@ -490,6 +116,9 @@ void GameMemoryReader::findIgnoredPlayersId(QStringList playersIdList)
 
     concurrency::parallel_for_each(numbers.begin(), numbers.end(), [&](DWORD64 n) {
         findedPlayersIdList = findIgnoredPlayersIdInMemorySection(n, n + step, playersIdList, hProcess);
+
+        if(m_abort == true)
+            return;
 
         if (!findedPlayersIdList.isEmpty() && !m_ignoredPlayersIdFinded)
         {
@@ -533,17 +162,19 @@ QString GameMemoryReader::findSteamSoulstormSessionId()
     if(hProcess==nullptr)
         return QString();
 
-    QByteArray buffer(/*30400*/100000, 0);
+    QByteArray buffer(100000, 0);
 
-    //unsigned long ptr1Count = 160116800;
-    unsigned long ptr1Count = /*100000000*/0x00000000;
-    while (ptr1Count < /*200000000*/0x7FFE0000)
+    unsigned long ptr1Count = 0x00000000;
+    while (ptr1Count < 0x7FFE0000)
     {
+        if(m_abort)
+            return QString();
+
 
         SIZE_T bytesRead = 0;
 
         // Если функция вернула не ноль, то продолжим цикл
-        if(!ReadProcessMemory(hProcess, (LPCVOID)ptr1Count, buffer.data(), /*30400*/100000 , &bytesRead))
+        if(!ReadProcessMemory(hProcess, (LPCVOID)ptr1Count, buffer.data(), 100000 , &bytesRead))
         {
             if(GetLastError()!=299)
             {
@@ -551,6 +182,9 @@ QString GameMemoryReader::findSteamSoulstormSessionId()
                 continue;
             }
         }
+
+        if(m_abort)
+            return QString();
 
         for (int i = 100; i < static_cast<int>(bytesRead) - 44; i++)
         {
@@ -596,7 +230,7 @@ QString GameMemoryReader::findDefinitiveEditionSessionId(DWORD64 startAdress, DW
 
     while (ptr1Count < ptr2Count)
     {
-        if(m_dataFinded)
+        if(m_dataFinded || m_abort)
             return QString();
 
         DWORD64  bytesRead = 0;
@@ -677,7 +311,7 @@ QStringList GameMemoryReader::findIgnoredPlayersIdInMemorySection(DWORD64 startA
 
     while (ptr1Count < ptr2Count)
     {
-        if(m_ignoredPlayersIdFinded)
+        if(m_ignoredPlayersIdFinded || m_abort)
             return QStringList();
 
         DWORD64  bytesRead = 0;
@@ -693,7 +327,7 @@ QStringList GameMemoryReader::findIgnoredPlayersIdInMemorySection(DWORD64 startA
 
         for (int i = 401; i < bytesRead - 401; i++)
         {
-            if(m_ignoredPlayersIdFinded)
+            if(m_ignoredPlayersIdFinded || m_abort)
                 return QStringList();
 
             bool match = true;
@@ -756,67 +390,6 @@ QStringList GameMemoryReader::findIgnoredPlayersIdInMemorySection(DWORD64 startA
             }
         }
 
-
-        /*for (int i = 0; i < bytesRead - 400; i++)
-        {
-            if(m_ignoredPlayersIdFinded)
-                return QStringList();
-
-            bool match = true;
-
-            for (int j = 0; j < matchStartMessageHeader.size(); j++)
-            {
-                if (buffer.at(i + j) != matchStartMessageHeader[j])
-                {
-                    match = false;
-                    break;
-                }
-            }
-
-            if (!match)
-                continue;
-
-            auto temp2 = buffer.mid(i, 400);
-
-            bool allIdFinded = true;
-
-            for(auto& needName2 : playerIdList)
-            {
-                if (!temp2.contains(needName2.toLocal8Bit()))
-                {
-                    allIdFinded = false;
-                    i+=400;
-                    break;
-                }
-            }
-
-            if(allIdFinded)
-            {
-                qDebug() << "Second matched" << temp2;
-
-                QStringList idList;
-                QString currentId;
-
-                for (auto& symbol : temp2)
-                {
-                    if (maskString.contains(symbol))
-                    {
-                        currentId.append(symbol);
-                    }
-                    else
-                    {
-                        if(currentId.size() == 8 && !idList.contains(currentId))
-                            idList.append(currentId);
-
-                        currentId.clear();
-                    }
-                }
-
-                if(idList.count() == playerIdList.count() + 1)
-                    return idList;
-            }
-        }*/
-
         ptr1Count += bufferSize;
     }
 
@@ -851,7 +424,7 @@ HANDLE GameMemoryReader::getProcessHandle(QString gameName)
 
 void GameMemoryReader::setGameType(GameType::GameTypeEnum newGameType)
 {
-    m_playersSteamScannerMutex.lock();
+    m_mutex.lock();
     m_gameType = newGameType;
-    m_playersSteamScannerMutex.unlock();
+    m_mutex.unlock();
 }
