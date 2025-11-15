46d09b682a3de0ab55ee090a29d362c405197f06
Evgeny <dubinaxdd@gmail.com>
2025-11-16 01:22:09 +0500

    Пилим нофог для ДЕ

--- dev/gameController/soulstormMemoryController/soulstormMemoryController.cpp
+++ dev/gameController/soulstormMemoryController/soulstormMemoryController.cpp
@@ -1,4 +1,8 @@
 #include <soulstormMemoryController.h>
+#include <TlHelp32.h>
+#include <windows.h>
+#include <psapi.h>
+#include <QtEndian>
 
 PVOID FogAddr = (PVOID)0x008282F0;
 PVOID MapSkyDistanceAddr = (PVOID)0x0082A33A;
@@ -17,11 +21,153 @@ BYTE temp6_2[6] = {0};
 
 BYTE temp4[6] = {0};
 
+/*uintptr_t GetModuleBaseAddress(DWORD processID, const std::string& moduleName) {
+    uintptr_t moduleBaseAddress = 0;
+    HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, processID);
+
+    if (hSnapshot != INVALID_HANDLE_VALUE) {
+        MODULEENTRY32 moduleEntry;
+        moduleEntry.dwSize = sizeof(MODULEENTRY32);
+
+        if (Module32First(hSnapshot, &moduleEntry)) {
+            do {
+                // Convert moduleEntry.szModule to std::string for comparison
+                std::string currentModuleName(moduleEntry.szModule);
+                if (currentModuleName == moduleName) {
+                    moduleBaseAddress = reinterpret_cast<uintptr_t>(moduleEntry.modBaseAddr);
+                    break;
+                }
+            } while (Module32Next(hSnapshot, &moduleEntry));
+        }
+        CloseHandle(hSnapshot);
+    }
+    return moduleBaseAddress;
+}*/
+
+uintptr_t GetModuleBaseAddress(DWORD processId, const std::string& moduleName) {
+    HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processId);
+    if (hProcess == NULL) {
+        qDebug() << "Failed to open process. Error: " << GetLastError();
+        return 0;
+    }
+
+    HMODULE hMods[1024];
+    DWORD cbNeeded;
+    if (EnumProcessModules(hProcess, hMods, sizeof(hMods), &cbNeeded)) {
+        for (unsigned int i = 0; i < (cbNeeded / sizeof(HMODULE)); i++) {
+            TCHAR szModName[MAX_PATH];
+            if (GetModuleFileNameEx(hProcess, hMods[i], szModName, sizeof(szModName) / sizeof(TCHAR))) {
+                std::string currentModuleName(szModName);
+                if (currentModuleName.find(moduleName) != std::string::npos) {
+                    MODULEINFO mi;
+                    if (GetModuleInformation(hProcess, hMods[i], &mi, sizeof(mi))) {
+                        CloseHandle(hProcess);
+                        return reinterpret_cast<uintptr_t>(mi.lpBaseOfDll);
+                    }
+                }
+            }
+        }
+    } else {
+        qDebug() << "Failed to enumerate modules. Error: " << GetLastError();
+    }
+
+    CloseHandle(hProcess);
+    return 0; // Module not found
+}
+
+
 SoulstormMemoryController::SoulstormMemoryController(SettingsController* settingsController, QObject *parent)
     : QObject(parent)
     , m_settingsController(settingsController)
 {
     QObject::connect(m_settingsController, &SettingsController::settingsLoaded, this, &SoulstormMemoryController::onSettingsLoaded,Qt::QueuedConnection);
+
+    //Получаем ID процесса игры
+    QString gameName = "Warhammer 40,000: Dawn of War";
+
+    QTextCodec *codec = QTextCodec::codecForName("UTF-8");
+    LPCWSTR lps = (LPCWSTR)gameName.utf16();
+    HWND m_gameHwnd = NULL;
+
+    m_gameHwnd = FindWindowW(NULL, lps);
+
+    if(!m_gameHwnd)
+        qDebug() << "Process Not Openned";
+
+    DWORD PID;
+    GetWindowThreadProcessId(m_gameHwnd, &PID);
+
+    //Получаем базовый адрес сим енжина
+    std::string targetModuleName = "SimEngine.dll";
+
+    uintptr_t baseAddress = GetModuleBaseAddress( PID, targetModuleName);
+
+    if (baseAddress != 0) {
+        qDebug() << "Base address of " << QString::fromStdString(targetModuleName) << " in current process: 0x" << hex << baseAddress;
+    } else {
+        qDebug() << "Module " << QString::fromStdString(targetModuleName) << " not found or error occurred.";
+    }
+
+
+    // Получение дескриптора процесса игры
+    HANDLE hProcess= OpenProcess(PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE, 0, PID);
+
+    if(hProcess==nullptr)
+        qDebug() << "Process handle not finded";
+
+
+    //Получаем адрес области памяти в которую пишет сименжин
+    uintptr_t indent = 0x138398; //отступ от начала сименжина
+    QByteArray simEngineMemoryAddressByteArray(8, 0);
+
+    ReadProcessMemory(hProcess, (PVOID)(baseAddress + indent), simEngineMemoryAddressByteArray.data(), 8, nullptr);
+    qDebug() << "SimEngineMemoryAddressByteArray: " << hex << (PVOID)(baseAddress + indent) << simEngineMemoryAddressByteArray.toHex().data();
+
+    quint64 simEngineMemoryAddress = qFromLittleEndian<quint64>((uchar*)simEngineMemoryAddressByteArray.data());
+    qDebug() << "SimEngineMemoryAddress" << hex << simEngineMemoryAddress;
+
+
+    //Получаем адрес объекта террейна
+    uintptr_t terrainIndent = 0x40; //отступ от начала памяти сименжина
+    QByteArray terrainAddressByteArray(8, 0);
+
+    ReadProcessMemory(hProcess, (PVOID)(simEngineMemoryAddress + terrainIndent), terrainAddressByteArray.data(), 8, nullptr);
+    qDebug() << "TerrainAddressByteArray: " << hex << (PVOID)(baseAddress + indent) << terrainAddressByteArray.toHex().data();
+
+    quint64 terrainAddress = qFromLittleEndian<quint64>((uchar*)terrainAddressByteArray.data());
+    qDebug() << "terrainAddressByteArray" << hex << terrainAddress;
+
+
+    //Получаем адреса память в которых будем менять
+    uintptr_t fogMinAddress = terrainAddress + 0xC44;
+    qDebug() << "fogMinAdress" << hex << fogMinAddress;
+
+    uintptr_t fogMaxAddress = terrainAddress + 0xC48;
+    qDebug() << "fogMaxAddress" << hex << fogMaxAddress;
+
+    uintptr_t skyDistanceAddress = terrainAddress + 0xC60;
+    qDebug() << "skyDistanceAddress" << hex << skyDistanceAddress;
+
+    uintptr_t skyRadius1_Address = terrainAddress + 0xC90;
+    qDebug() << "skyRadius1_Address" << hex << skyRadius1_Address;
+
+    uintptr_t skyRadius2_Address = terrainAddress + 0xC68;
+    qDebug() << "skyRadius2_Address" << hex << skyRadius2_Address;
+
+    BYTE fogMinValue[4] = {0x00, 0x00, 0xCE, 0x43};
+    WriteProcessMemory(hProcess, (PVOID)fogMinAddress, fogMinValue, 4, nullptr); //default 50, new 412  [00 00 CE 43]
+
+    BYTE fogMaxValue[4] = {0x00, 0x00, 0x00, 0x44};
+    WriteProcessMemory(hProcess, (PVOID)fogMaxAddress, fogMaxValue, 4, nullptr); //default 50, new 512  [00 00 00 44]
+
+    BYTE skyDistanceValue[4] = {0x00, 0x00, 0x80, 0x44};
+    WriteProcessMemory(hProcess, (PVOID)skyDistanceAddress, skyDistanceValue, 4, nullptr); //default 160, new 1024  [00 00 80 44]
+
+    BYTE skyRadius1_Value[4] = {0x00, 0x00, 0x7A, 0x44};
+    WriteProcessMemory(hProcess, (PVOID)skyRadius1_Address, skyRadius1_Value, 4, nullptr); //default 246.9650116, new 1000 [00 00 7A 44]
+
+    BYTE skyRadius2_Value[4] = {0x00, 0x00, 0x7A, 0x44};
+    WriteProcessMemory(hProcess, (PVOID)skyRadius2_Address, skyRadius2_Value, 4, nullptr); //default 246.9650116, new 1000 [00 00 7A 44]
 }
 
 void SoulstormMemoryController::onGameLaunchStateChanged(bool state)
@@ -105,3 +251,4 @@ void SoulstormMemoryController::onNoFogStateChanged(bool state)
     //}
     CloseHandle(hProcess);
 }
+
